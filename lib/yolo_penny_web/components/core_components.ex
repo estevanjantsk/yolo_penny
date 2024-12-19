07ffbc99cad9cf242e8a22946710e64ae5687e60
Estevan Jantsk <estevan.jantsk@scoro.com>
2024-12-19 17:52:08 +0200

    Refactor input function to use alias for Form module

--- lib/yolo_penny_web/components/core_components.ex
+++ lib/yolo_penny_web/components/core_components.ex
@@ -17,6 +17,7 @@ defmodule YoloPennyWeb.CoreComponents do
   use Phoenix.Component
   use Gettext, backend: YoloPennyWeb.Gettext
 
+  alias Phoenix.HTML.Form
   alias Phoenix.LiveView.JS
 
   @doc """
@@ -304,7 +305,7 @@ defmodule YoloPennyWeb.CoreComponents do
   def input(%{type: "checkbox"} = assigns) do
     assigns =
       assign_new(assigns, :checked, fn ->
-        Phoenix.HTML.Form.normalize_value("checkbox", assigns[:value])
+        Form.normalize_value("checkbox", assigns[:value])
       end)
 
     ~H"""
