dd6cf9af2c1fd2b3b60b6f4833e7e9ec15ad6120
Estevan Jantsk <estevan.jantsk@scoro.com>
2024-12-19 17:04:12 +0200

    Add Sobelow for security analysis in development and test environments

--- mix.exs
+++ mix.exs
@@ -55,7 +55,8 @@ defmodule YoloPenny.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false}
     ]
   end
 
