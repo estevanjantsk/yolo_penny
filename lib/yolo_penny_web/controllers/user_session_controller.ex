80db2dfd2039d9edfc4b67077ac10294a0cb3752
Estevan Jantsk <estevan.jantsk@scoro.com>
2024-12-20 17:12:19 +0200

    Refactor user session handling and enhance login UI

--- lib/yolo_penny_web/controllers/user_session_controller.ex
+++ lib/yolo_penny_web/controllers/user_session_controller.ex
@@ -8,23 +8,36 @@ defmodule YoloPennyWeb.UserSessionController do
         conn,
         %{"_action" => "registered", "registration" => %{"username" => username}} = _params
       ) do
+    create(conn, username, "Account created successfully!", "/users/register")
+  end
+
+  def create(
+        conn,
+        %{
+          "user" => %{"username" => username}
+        } = _params
+      ) do
+    create(conn, username, "Welcome back!", "/users/log_in")
+  end
+
+  defp create(conn, username, info, redirect_to) do
     case Users.find_user(username) do
       {:ok, user} ->
         conn
-        |> put_flash(:info, "Account created successfully!")
+        |> put_flash(:info, info)
         |> UserAuth.log_in_user(user)
         |> redirect(to: "/dashboard")
 
       {:error, :user_not_found} ->
         conn
-        |> put_flash(:info, "Something went wrong!")
-        |> redirect(to: "/")
+        |> put_flash(:error, "Something went wrong!")
+        |> redirect(to: redirect_to)
     end
   end
 
   def delete(conn, _params) do
     conn
-    |> put_flash(:info, "Logged out successfully.")
+    |> put_flash(:info, "Logged out.")
     |> UserAuth.log_out_user()
     |> redirect(to: "/users/log_in")
   end
