815d577b3181662f4cb8d5f0cfe5aeb8e73cd6c1
Estevan Jantsk <estevan.jantsk@scoro.com>
2024-12-21 21:43:08 +0200

    add expenses api

---
+++ lib/yolo_penny/expenses.ex
@@ -0,0 +1,17 @@
+defmodule YoloPenny.Expenses do
+  @moduledoc """
+  This module provides functions for managing expenses.
+  Expenses API.
+  """
+
+  alias YoloPenny.Expenses.ExpenseServer
+
+  def add_expense(user_id, expense), do: ExpenseServer.add_expense(user_id, expense)
+
+  def get_expenses_by_user(user_id), do: ExpenseServer.get_expenses(user_id)
+
+  def delete_expense_by_user(user_id, expense_id),
+    do: ExpenseServer.delete_expense(user_id, expense_id)
+
+  def clean_expenses, do: ExpenseServer.clean()
+end
