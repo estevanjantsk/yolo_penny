eb16c83705a30c891ae27a0a73bf669f24fd9bb7
Estevan Jantsk <estevan.jantsk@scoro.com>
2024-12-22 03:49:23 +0300

    add form component for new expense creation and update dashboard logic

--- lib/yolo_penny_web/live/dashboard_live.ex
+++ lib/yolo_penny_web/live/dashboard_live.ex
@@ -5,9 +5,7 @@ defmodule YoloPennyWeb.DashboardLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    user_id = socket.assigns.current_user.id
-    {:ok, expenses} = Expenses.get_expenses_by_user(user_id)
-    {:ok, stream(socket, :expenses, expenses)}
+    {:ok, socket |> assign_expenses}
   end
 
   @impl true
@@ -21,11 +19,19 @@ defmodule YoloPennyWeb.DashboardLive do
     <.title_bar>
       My Expenses
       <:actions>
-        <.button id="expense-btn">
+        <.button
+          id="expense-btn"
+          phx-click={JS.push("new")}
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
           <.icon name="hero-add-new" /><span class="ml-2">New Expense</span>
         </.button>
       </:actions>
     </.title_bar>
+
+    <div class="bg-gray-200 text-gray-700 p-2 mt-2">
+      Total: 100
+    </div>
 
     <.table id="expenses" rows={@streams.expenses}>
       <:col :let={{_id, expense}} label="Description"><%= expense.description %></:col>
@@ -40,19 +46,29 @@ defmodule YoloPennyWeb.DashboardLive do
         </.link>
       </:action>
     </.table>
+
+    <.modal :if={@live_action == :new} id="expense-modal" show on_cancel={JS.patch(~p"/dashboard")}>
+      <.live_component
+        module={YoloPennyWeb.DashboardLive.FormComponent}
+        id={:new}
+        title={@page_title}
+        action={@live_action}
+        patch={~p"/dashboard"}
+      />
+    </.modal>
     """
   end
 
   defp apply_action(socket, :new, _params) do
     socket
     |> assign(:page_title, "New Expense")
-    |> assign(:product, %Expense{})
+    |> assign(:expense, %Expense{})
   end
 
   defp apply_action(socket, :index, _params) do
     socket
     |> assign(:page_title, "My Expenses")
-    |> assign(:product, nil)
+    |> assign(:expense, nil)
   end
 
   @impl true
@@ -63,5 +79,17 @@ defmodule YoloPennyWeb.DashboardLive do
     {:ok, _} = Expenses.delete_expense_by_user(user_id, id)
 
     {:noreply, stream_delete(socket, :expenses, expense)}
+  end
+
+  @impl true
+  def handle_event("new", _, socket) do
+    {:noreply, push_patch(socket, to: ~p"/dashboard/new-expense")}
+  end
+
+  def assign_expenses(socket) do
+    user_id = socket.assigns.current_user.id
+    {:ok, expenses} = Expenses.get_expenses_by_user(user_id)
+
+    stream(socket, :expenses, expenses)
   end
 end
